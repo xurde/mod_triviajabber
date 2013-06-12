3d5f24dc1b2ff1ff2177e8be88492c790f2fc171
bronzeboyvn <htk@htk.(none)>
2013-06-12 16:46:57 +0700

    build script

---
+++ build_triviapad.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+CURR=${PWD}
+
+$CURR/../../../bin/erlc permutation.erl && $CURR/../../../bin/erlc triviajabber_question.erl && $CURR/../../../bin/erlc triviajabber_store.erl && $CURR/../../../bin/erlc -I $CURR/../include/ player_store.erl && $CURR/../../../bin/erlc -I $CURR/../include/ -I $CURR/../../stdlib-1.17.5/include/ $CURR/mod_triviajabber.erl && $CURR/../../../bin/erlc -I $CURR/../include/ triviajabber_game.erl
